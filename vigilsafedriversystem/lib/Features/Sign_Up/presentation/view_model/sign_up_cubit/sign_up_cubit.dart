bb713027f61915a6c6df199c4c48b72c3259213e
islam <heshamislam556@gmail.com>
2024-05-16 03:55:06 +0200

    Update compileSdkVersion to 34 and add Firebase Auth dependency
    This commit updates the compileSdkVersion to 34 in the build.gradle file and adds
    the Firebase Auth dependency in the SignUpCubit file for signing up users.

--- vigilsafedriversystem/lib/Features/Sign_Up/presentation/view_model/sign_up_cubit/sign_up_cubit.dart
+++ vigilsafedriversystem/lib/Features/Sign_Up/presentation/view_model/sign_up_cubit/sign_up_cubit.dart
@@ -1,8 +1,24 @@
-import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'sign_up_state.dart';
 
 class SignUpCubit extends Cubit<SignUpState> {
   SignUpCubit() : super(SignUpInitial());
+
+  Future signUp(String email, String password, BuildContext context) async {
+    emit(SignUpLoading());
+    await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(email: email, password: password)
+        .then((val) {
+      if (val.user != null) {
+        val.user!.sendEmailVerification().then((_) {
+          emit(SignUpSuccess());
+        }).catchError((error) {
+          emit(SignUpFailure(errorMessage: error));
+        });
+      }
+    });
+  }
 }
