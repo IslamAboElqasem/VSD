a792d77754c0fac30f5f6ced9a45c57a65765f85
islam <heshamislam556@gmail.com>
2024-06-03 00:59:58 +0200

    Add driver view and driver view body widgets

---
+++ vigilsafedriversystem/lib/Features/Driver/presentation/views/driver_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:vigilsafedriversystem/Features/Driver/presentation/views/widgets/driver_view_body.dart';
+
+class DriverView extends StatelessWidget {
+  const DriverView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: DriverViewBody(),
+    );
+  }
+}
---
+++ vigilsafedriversystem/lib/Features/Driver/presentation/views/widgets/driver_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DriverViewBody extends StatelessWidget {
+  const DriverViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
