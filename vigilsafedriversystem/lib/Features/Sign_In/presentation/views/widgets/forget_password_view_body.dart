71fa169e4ad4252669a8de58b5c38f1dcb1f1382
islam <heshamislam556@gmail.com>
2024-06-03 02:25:56 +0200

    Refactor forget password view body widget
    This commit refactors the forget password view body widget by adding a background pattern image and adjusting the layout of the text form field and reset password button. The code has also been cleaned up and made more readable.

--- vigilsafedriversystem/lib/Features/Sign_In/presentation/views/widgets/forget_password_view_body.dart
+++ vigilsafedriversystem/lib/Features/Sign_In/presentation/views/widgets/forget_password_view_body.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:vigilsafedriversystem/core/widgets/show_toast.dart';
 
 class ForgetPasswordViewBody extends StatefulWidget {
-  ForgetPasswordViewBody({super.key});
+  const ForgetPasswordViewBody({super.key});
 
   @override
   State<ForgetPasswordViewBody> createState() => _ForgetPasswordViewBodyState();
@@ -15,53 +15,63 @@ class _ForgetPasswordViewBodyState extends State<ForgetPasswordViewBody> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xFF1589FF),
-        title: const Text('Forgot Password'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10))),
-              ),
-              controller: email,
+      body: Stack(children: [
+        Positioned.fill(
+          child: Opacity(
+            opacity: 0.2,
+            child: Image.asset(
+              'assets/cars_background.jpeg', // Background pattern image
+              fit: BoxFit.cover,
             ),
-            const SizedBox(height: 20),
-            SizedBox(
-              width: 230,
-              height: 50,
-              child: ElevatedButton(
-                onPressed: () {
-                  try {
-                    assert(email.text
-                        .contains(RegExp(r'\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6}')));
-                    FirebaseAuth.instance
-                        .sendPasswordResetEmail(email: email.text)
-                        .then((value) {
-                      showToast(context, 'Done Send', Colors.green);
-                      Navigator.pop(context);
-                    });
-                  } catch (e) {
-                    showToast(context, 'this no\'t E-Mail', Colors.redAccent);
-                  }
-                },
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black,
-                  backgroundColor: Colors.blue,
-                  shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20))),
-                ),
-                child: const Text('Reset Password'),
-              ),
-            ),
-          ],
+          ),
         ),
-      ),
+        Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 100,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                ),
+                controller: email,
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    try {
+                      assert(email.text
+                          .contains(RegExp(r'\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6}')));
+                      FirebaseAuth.instance
+                          .sendPasswordResetEmail(email: email.text)
+                          .then((value) {
+                        showToast(context, 'Done Send', Colors.green);
+                        Navigator.pop(context);
+                      });
+                    } catch (e) {
+                      showToast(context, 'this no\'t E-Mail', Colors.redAccent);
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    backgroundColor: Colors.blue,
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20))),
+                  ),
+                  child: const Text('Reset Password'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ]),
     );
   }
 }
