817bdaaad1fb645bee59ec7003d4906b39422903
islam <heshamislam556@gmail.com>
2024-06-10 01:28:11 +0200

    Add background image and location functionality to driver view body widget

--- vigilsafedriversystem/lib/Features/Driver/presentation/views/widgets/driver_view_body.dart
+++ vigilsafedriversystem/lib/Features/Driver/presentation/views/widgets/driver_view_body.dart
@@ -36,75 +36,86 @@ class _DriverViewBodyState extends State<DriverViewBody> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 25),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              alignment: Alignment.center,
-              height: 50,
-              child: const Text(
-                'Entre your Tracker ',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+      body: Stack(children: [
+        Positioned.fill(
+          child: Image.asset(
+            'assets/driverBackGround.jpeg', // Background pattern image
+            opacity: const AlwaysStoppedAnimation(.3),
+            fit: BoxFit.cover,
+            height: double.infinity,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 100),
+              Container(
+                alignment: Alignment.center,
+                height: 50,
+                child: const Text(
+                  'Entre your Tracker ',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10))),
+              const SizedBox(
+                height: 15,
               ),
-              controller: trackerEmail,
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            _position != null
-                ? Container(
-                    height: 100,
-                    alignment: Alignment.center,
-                    child: Text(
-                        'Current location:\nlatitude: ${_position!.latitude.toString()}\nlongitude: ${_position!.longitude.toString()}  '),
-                  )
-                : Container(
-                    height: 100,
-                    alignment: Alignment.center,
-                    child: const Text(
-                      'tap to get your location',
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                ),
+                controller: trackerEmail,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              _position != null
+                  ? Container(
+                      height: 100,
+                      alignment: Alignment.center,
+                      child: Text(
+                          'Current location:\nlatitude: ${_position!.latitude.toString()}\nlongitude: ${_position!.longitude.toString()}  '),
+                    )
+                  : Container(
+                      height: 100,
+                      alignment: Alignment.center,
+                      child: const Text(
+                        'tap to get your location',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+              const SizedBox(
+                height: 15,
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    getCurrentLocation();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    backgroundColor: Colors.blue,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
                     ),
                   ),
-            const SizedBox(
-              height: 15,
-            ),
-            SizedBox(
-              width: double.infinity,
-              height: 50,
-              child: ElevatedButton(
-                onPressed: () {
-                  getCurrentLocation();
-                },
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black,
-                  backgroundColor: Colors.blue,
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  child: const Text(
+                    'Get My Location',
+                    style: TextStyle(color: Colors.white),
                   ),
                 ),
-                child: const Text(
-                  'Get My Location',
-                  style: TextStyle(color: Colors.white),
-                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
+      ]),
     );
   }
 }
