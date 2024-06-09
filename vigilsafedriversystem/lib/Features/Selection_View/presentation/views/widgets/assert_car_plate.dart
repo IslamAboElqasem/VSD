f96f3beebc1695a9d30d9d47ce3fdd71335a61ca
islam <heshamislam556@gmail.com>
2024-06-09 05:30:08 +0200

    Add input validation to assertCarPlate dialog in DriverViewBody

--- vigilsafedriversystem/lib/Features/Selection_View/presentation/views/widgets/assert_car_plate.dart
+++ vigilsafedriversystem/lib/Features/Selection_View/presentation/views/widgets/assert_car_plate.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:vigilsafedriversystem/Features/Driver/presentation/views/widgets/driver_view_body.dart';
+import 'package:vigilsafedriversystem/core/widgets/show_toast.dart';
 
 void assertCarPlate(BuildContext context) {
-  TextEditingController textFieldController = TextEditingController();
+  final TextEditingController textFieldController = TextEditingController();
 
   showDialog(
     context: context,
     builder: (BuildContext context) {
       return AlertDialog(
-        backgroundColor: Colors.white,
         title: const Text('Enter Car Plate'),
         content: TextField(
           controller: textFieldController,
@@ -16,13 +16,25 @@ void assertCarPlate(BuildContext context) {
         ),
         actions: <Widget>[
           TextButton(
+            child: const Text('Cancel'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          TextButton(
             child: const Text('OK'),
             onPressed: () {
-              Navigator.pushReplacement(
+              if (textFieldController.text.isEmpty) {
+                showToast(context, 'Car plate cannot be empty', Colors.red);
+              } else {
+                // Navigate to DriverViewBody if input is valid
+                Navigator.pushReplacement(
                   context,
                   MaterialPageRoute(
                     builder: (context) => const DriverViewBody(),
-                  ));
+                  ),
+                );
+              }
             },
           ),
         ],
