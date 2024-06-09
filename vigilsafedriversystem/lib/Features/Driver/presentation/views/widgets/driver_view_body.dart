f2cb5dec8d05b3235cc5f66b3f5f50aa6f5916a7
islam <heshamislam556@gmail.com>
2024-06-09 17:11:39 +0200

    Refactor driver view body widget
    This commit refactors the driver view body widget by adding a text form field for entering the tracker email, and a button for getting the current location. The layout of the widget has also been adjusted to improve readability.

--- vigilsafedriversystem/lib/Features/Driver/presentation/views/widgets/driver_view_body.dart
+++ vigilsafedriversystem/lib/Features/Driver/presentation/views/widgets/driver_view_body.dart
@@ -10,7 +10,7 @@ class DriverViewBody extends StatefulWidget {
 
 class _DriverViewBodyState extends State<DriverViewBody> {
   Position? _position;
-
+  TextEditingController trackerEmail = TextEditingController();
   void getCurrentLocation() async {
     Position position = await determinePosition();
 
@@ -36,17 +36,74 @@ class _DriverViewBodyState extends State<DriverViewBody> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: _position != null
-              ? Text(
-                  'Current location:\nlatitude: ${_position!.latitude.toString()}\nlongitude: ${_position!.longitude.toString()}  ')
-              : const Text('tap to get your location')),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          getCurrentLocation();
-        },
-        tooltip: 'get my location',
-        child: const Icon(Icons.location_city),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 25),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              alignment: Alignment.center,
+              height: 50,
+              child: const Text(
+                'Entre your Tracker ',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+              ),
+              controller: trackerEmail,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            _position != null
+                ? Container(
+                    height: 100,
+                    alignment: Alignment.center,
+                    child: Text(
+                        'Current location:\nlatitude: ${_position!.latitude.toString()}\nlongitude: ${_position!.longitude.toString()}  '),
+                  )
+                : Container(
+                    height: 100,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      'tap to get your location',
+                      style:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+            const SizedBox(
+              height: 15,
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+                  getCurrentLocation();
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.black,
+                  backgroundColor: Colors.blue,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                ),
+                child: const Text(
+                  'Get My Location',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
